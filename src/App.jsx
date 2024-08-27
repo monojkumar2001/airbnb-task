5bcbc697dd1358247df1ada93011708701e4db14
monoj kumar <monojkumar57859893@gmail.com>
2024-08-27 20:33:53 +0600

    asfdsa

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import './app.css'
+
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
 
 function App() {
-
   return (
     <>
-    <BrowserRouter>
+      <BrowserRouter>
         <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
@@ -16,7 +15,7 @@ function App() {
         <Footer />
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
